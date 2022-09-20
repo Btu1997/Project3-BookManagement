1acc71c6ffa67254a8418aef773803c26ed5274b
gurubhaskary <syguru82sun@gmail.com>
2022-09-20 13:58:36 +0530

    model Update

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,14 +1,14 @@
-const express= require('express');
-const router= express.router();
-//=============================================================================================================
-
-
-
-
-
+//=====================Importing Module and Packages=====================//
+const express = require('express');
+const router = express.Router();
 
 
 router.all("/*", (req, res) => { res.status(404).send({ status: false, error: " / invalid - path params - provided / " }); });
 
 
-module.exports = router;
+
+
+
+
+//=====================Module Export=====================//
+module.exports = router;   
