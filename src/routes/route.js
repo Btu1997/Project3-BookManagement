6856e03b5614645e98fb57a3949e721486acd6cb
Btu1997 <bittumishra08223@gmail.com>
2022-09-21 23:05:44 +0530

    middleware

--- src/routes/route.js
+++ src/routes/route.js
@@ -4,12 +4,14 @@ const router = express.Router();
 const {createUser,login} = require("../Controller/userController");
 const {createBooks,getAllBook,getBooksByPathParam} = require("../Controller/bookController");
 const {bookValidation} = require("../Validator/validate")
+const {authentication,authorisation} = require("../middleware/auth")
+
 
 router.post("/register", createUser);
 
 router.post("/login", login);
 
-router.post("/books",bookValidation, createBooks);
+router.post("/books",bookValidation,authentication,authorisation, createBooks);
 router.get("/books",getAllBook);
 router.get("/books/:bookId",getBooksByPathParam);
 
