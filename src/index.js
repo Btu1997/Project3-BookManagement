e6040c569e938835197f68665135bc1f2c322c61
Btu1997 <bittumishra08223@gmail.com>
2022-09-21 12:07:51 +0530

    error fix

--- src/index.js
+++ src/index.js
@@ -24,32 +24,3 @@ app.use('/', route)
 app.listen(process.env.PORT || 3000, function () {
     console.log('Express App Running on Port: ' + (process.env.PORT || 3000))
 });
-<<<<<<< HEAD
-=======
-
-
-//===================== Global Middleware for Console the Date, Time, IP Address and Print the perticular API Route Name when you will hit that API =====================//
-// app.use(
-//     function globalMiddleWare(req, res, next) {
-//         const today = moment();
-//         const formatted = today.format('YYYY-MM-DD hh:mm:ss');
-//         console.log("----------------")
-//         console.log("Date:-", formatted);
-//         console.log("IP Address:-", req.ip);
-//         console.log("API Route Info:-", req.originalUrl);
-//         next()
-//     }
-// )
-
-
-
-//===================== It will Handle error When You input Wrong Route =====================//
-// app.use(function (req, res) {
-//     var err = new Error("Not Found.")
-//     err.status = 404
-//     return res.status(404).send({ status: "404", msg: "Path not Found." })
-// })
-
-
-
->>>>>>> bb5f255 ( create Api)
